2b37cfb270b9c163b444a11168df8f1ef9948f92
7oa <semenova.7oa@yandex.ru>
2017-04-06 16:01:26 +0300

    callback form edit

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -7,16 +7,20 @@ $(document).ready(function() {
 			function(){
 				$('#callback')
 					.css('display', 'block')
-					.animate({opacity: 1, top: '10px'}, 200);
+					.animate({opacity: 1}, 200);
+				$('.callback-inner').animate({"margin-top": "30px"}, 200);
 			});
+		$("body").css('overflow-y','hidden');
 	});
 	$('#callback__close, #callback__overlay').click( function(){
 		$('#callback')
-			.animate({opacity: 0, top: '0'}, 200,
+			.animate({opacity: 0}, 200,
 				function(){
 					$(this).css('display', 'none');
 					$('#callback__overlay').fadeOut(400);
 				}
 			);
+		$('.callback-inner').animate({"margin-top": "0px"}, 200);
+		$("body").css('overflow-y','auto');
 	});
 });
